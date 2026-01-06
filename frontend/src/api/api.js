c66ff9eeba7ade6b98163ac3b544bd114a8164b8
jagruthip2007 <jagruthip2007@gmail.com>
2026-01-06 20:31:38 +0530

    api route made

---
+++ frontend/src/api/api.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: import.meta.env.BACKEND_URL,
+});
+
+export const getUsers = () => api.get("/api/users");
+
+export const createUser = (name, timezone) =>
+  api.post("/api/users", { name, timezone });
+
+export const updateUserTimezone = (userId, timezone) =>
+  api.put(`/api/users/${userId}/timezone`, { timezone });
+
+export const createEvent = (data) => api.post("/api/events", data);
+
+export const getEvents = (userId) =>
+  api.get("/api/events", {
+    params: { userId },
+  });
+
+export const updateEvent = (eventId, data) =>
+  api.put(`/api/events/${eventId}`, data);
+
+export const getEventLogs = (eventId) => api.get(`/api/events/${eventId}/logs`);
+
+export default api;
